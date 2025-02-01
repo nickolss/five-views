3c72f411741c58a4bed70c1ef832fc7eb4abcfc3
nickolss <nickolasmaiaraujo@gmail.com>
2025-02-01 18:24:09 -0300

    feat: Added project structure

---
+++ src/main/kotlin/com/github/nickolss/five_views/FiveViewsApplication.kt
@@ -0,0 +1,11 @@
+package com.github.nickolss.five_views
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FiveViewsApplication
+
+fun main(args: Array<String>) {
+	runApplication<FiveViewsApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/nickolss/five_views/FiveViewsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.nickolss.five_views
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FiveViewsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
